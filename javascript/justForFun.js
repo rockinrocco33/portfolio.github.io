39954d1bacb06ba15643adf8da513c0ef320ee02
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-03 19:01:51 -0400

    big update

---
+++ javascript/justForFun.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+	
+
+	$("#fun").addClass("active");
+
+
+});
+
+	$(document).bind('mousemove', function(e){
+
+ 	if(e.pageX < $("#navbar").position().left + $("#navbar").width() - 165
+		&& e.pageX > $("#navbar").position().left + 100
+
+		)
+	{
+    $('#mattRocco').css({
+        left: e.pageX -100,
+    }); 
+    $('#bottomNav').css({
+    	left: e.pageX - 140,
+    });
+
+}
+})
+
+
---
+++ javascript/pastProjects.js
@@ -0,0 +1,25 @@
+$(document).ready( function() {
+	
+
+	$("#pastprojects").addClass("active");
+
+
+});
+
+	$(document).bind('mousemove', function(e){
+
+ 	if(e.pageX < $("#navbar").position().left + $("#navbar").width() - 165
+		&& e.pageX > $("#navbar").position().left + 100
+
+		)
+	{
+    $('#mattRocco').css({
+        left: e.pageX -100,
+    }); 
+    $('#bottomNav').css({
+    	left: e.pageX - 140,
+    });
+
+}
+})
+
