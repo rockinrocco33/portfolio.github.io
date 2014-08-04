3d6aa1d7ee40c9e1167aee45d37c21eb61a4d3f6
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-04 02:20:34 -0400

    big update

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -2,9 +2,25 @@ $(document).ready( function() {
 	
     $('#mattRocco').hide(); 
     $('#bottomNav').hide();
+var name = location.pathname.split('/').slice(-1);
+name = String(name);
+var pos = name.indexOf('.');
+name = name.substring(0,pos);
+	var current = $("#" + name);
+	if(current.position().left){
+	current.addClass("active");
+	$('#mattRocco').css({
+        left:current.position().left ,
+    });
+    $('#bottomNav').css({
+    	left:current.position().left,
+    })
 
+    $('#mattRocco').show(); 
+    $('#bottomNav').show();
 
-});
+}
+})
 
 	$(document).bind('mousemove', function(e){
     $('#mattRocco').show(); 
