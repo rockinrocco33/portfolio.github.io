2db5dd96c468a275ee88545270b5ffe68e1e20ae
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-04 13:10:08 -0400

    nav

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -9,6 +9,7 @@ var pos = name.indexOf('.');
 name = name.substring(0,pos);
 	var current = $("#" + name);
          $("#" + name).addClass("active");
+         alert(name);
     document.getElementById(name).setAttribute("class","active");
 
 	if( $("#" + name)!= null){ 
