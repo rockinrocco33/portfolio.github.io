2164ffcff5742160e59cb2afcb06b015c3c44e3b
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-05 09:25:10 -0400

    chrome lag fix

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -29,7 +29,20 @@ name = name.substring(0,pos);
     $('#bottomNav').show();
 
 } 
-
+$.fn.disableSelection = function() {
+  return this.each(function() {
+    $(this).attr('unselectable', 'on')
+    .css({
+      '-moz-user-select':'none',
+      '-webkit-user-select':'none',
+      'user-select':'none',
+      '::selection':'none',
+    })
+    .each(function() {
+      this.onselectstart = function() { return false; };
+    });
+  });
+};
 
 	$(document).bind('mousemove', function(e){
     $('#mattRocco').show(); 
