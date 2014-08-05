22947e75127b652e701c3935f58bbf8b753c7202
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-05 10:52:46 -0400

    chrome lag fix

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -1,8 +1,9 @@
 $(document).ready( function() {
 	$("#navbarLoad").load("./navbar.html", function(){
-
-    $('#mattRocco').hide(); 
-    $('#bottomNav').hide();
+var mattrocco = $('#mattRocco');
+var bottomNav = $('#bottomNav');
+   mattrocco.hide(); 
+   bottomNav.hide();
 var name = location.pathname.split('/').slice(-1);
 name = String(name);
 var pos = name.indexOf('.');
@@ -11,58 +12,44 @@ name = name.substring(0,pos);
          $("#" + name).addClass("active");
          if(name.length == 0){
     document.getElementById("index").setAttribute("class","active");
-    $('#mattRocco').show(); 
-    $('#bottomNav').show();
+    mattrocco.show(); 
+    bottomNav.show();
 } else{
     document.getElementById(name).setAttribute("class","active");
 }
 	if( $("#" + name)!= null){ 
 	 $("#" + name).addClass("active");
-	// $('#mattRocco').css({
- //        left: $("#" + name).position().left ,
- //    });
- //    $('#bottomNav').css({
- //    	left: $("#" + name).position().left,
- //    })
-
-    $('#mattRocco').show(); 
-    $('#bottomNav').show();
+        mattrocco.css({
+        left: $("#"+name).position().left,
+    }); 
+    bottomNav.css({
+        left: $("#"+name).position().left,
+    });
+    mattrocco.show(); 
+    bottomNav.show();
 
 } 
-$.fn.disableSelection = function() {
-  return this.each(function() {
-    $(this).attr('unselectable', 'on')
-    .css({
-      '-moz-user-select':'none',
-      '-webkit-user-select':'none',
-      'user-select':'none',
-      '::selection':'none',
-    })
-    .each(function() {
-      this.onselectstart = function() { return false; };
-    });
-  });
-};
 
 var navbar = $('#navbar');
-var mattrocco = $('#mattRocco');
-var bottomNav = $('#bottomNav');
+
 var pageX;
+var newMatt;
+var newBot
 	$(document).bind('mousemove', function(e){
         console.log("moved");
 
     if(navbar!=null){
         pageX= e.pageX
  	if(pageX < navbar.position().left + navbar.width() - 165
-		&& pageX > navbar.position().left + 100
-
-		)
+		&& pageX > navbar.position().left + 100)
 	{
+        newMatt = pageX-100;
+        newBot = pageX-140
     mattrocco.css({
-        left: pageX -100,
+        left: newMatt,
     }); 
     bottomNav.css({
-    	left: pageX - 140,
+    	left: newBot,
     });
             console.log("completed");
 
